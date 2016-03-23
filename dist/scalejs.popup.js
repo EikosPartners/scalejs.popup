b63f72a7e4617faac69e5039140678bb16af7a2d
wzamlynny <wzamlynny@gmail.com>
2016-03-23 11:43:48 -0400

    Added background, postion, and root option

--- dist/scalejs.popup.js
+++ dist/scalejs.popup.js
@@ -1,14 +1,16 @@
 
-define('text!scalejs.popup/popup.html',[],function () { return '<div id="popup_template">\r\n    <div class="popup-container" id="popup-container" data-bind="css: { \'popup-hidden\' : !popupVisible() }">\r\n        <!-- ko if: modal -->\r\n            <div class="popup-background"></div>\r\n        <!-- /ko -->\r\n        <!-- ko ifnot: modal -->\r\n            <div class="popup-background" data-bind="click: hidePopup"></div>\r\n        <!-- /ko -->\r\n        <div class="popup-box">\r\n        <!-- ko render: popupRegion -->\r\n        <!-- /ko -->\r\n        </div>\r\n    </div>\r\n</div>\r\n\r\n<div id="blank_popup_template">\r\n    <div>\r\n    </div>\r\n</div>\r\n';});
+define('text!scalejs.popup/popup.html',[],function () { return '<div id="popup_template">\r\n    <div class="popup-container" id="popup-container" data-bind="css: { \'popup-hidden\' : !popupVisible() }">\r\n        <!-- ko if: modal -->\r\n            <div data-bind="css: background "></div>\r\n        <!-- /ko -->\r\n        <!-- ko ifnot: modal -->\r\n            <div data-bind="css: background, click: hidePopup"></div>\r\n        <!-- /ko -->\r\n        <div class="popup-box" data-bind="css: position"> \r\n        <!-- ko render: popupRegion -->\r\n        <!-- /ko -->\r\n        </div>\r\n    </div>\r\n</div>\r\n\r\n<div id="blank_popup_template">\r\n    <div>\r\n    </div>\r\n</div>\r\n';});
 
 
 define('scalejs.popup',[
     'scalejs!core',
     'knockout',
+    'module',
     'scalejs.mvvm.views!scalejs.popup/popup'
 ], function (
     core,
-    ko
+    ko,
+    module
 ) {
     'use strict';
 
@@ -20,17 +22,55 @@ define('scalejs.popup',[
         popupVisible = observable(false),
         popupRegion = observable(),
         modal = observable(),
-        viewModel;
+        background = observable(),
+        postion = observable(),
+        viewModel,
+        config = module.config;
 
     function hidePopup () {
         popupVisible(false);
         popupRegion(undefined);
+        
+        if (!config.doNotRender) {
+            var parent = document.body;
+            var oldRoot = document.getElementById("popupRoot");
+            parent.removeChild(oldRoot);
+        }
     }
 
     function renderPopup (template) {
         template.template.data.modal ? modal(true) : modal(false);
+        if (template.template.data.background === true || template.template.data.background === undefined) {
+            background('popup-background');
+        }
+        else {
+            background('popup-no-background');
+        }
+        var pos = template.template.data.position;
+        if (pos !== undefined) {
+            position('popup-box-' + pos);
+        }
+        else {
+            position('popup-box-center');
+        }
         popupRegion(template);
         popupVisible(true);
+        
+        if (!config.doNotRender) {
+            if (document.getElementById("popupRoot") === null) {
+                var newRoot = document.createElement("span");
+                newRoot.setAttribute("id","popupRoot");
+                newRoot.setAttribute("data-bind","render: popupRoot");
+                if (document.body === undefined || document.body.children === undefined) {
+                    return;
+                }
+                var parent = document.body;
+                var node = parent.children[0];
+                parent.insertBefore(newRoot, node);
+                
+                ko.applyBindings(this, document.getElementById("popupRoot"));
+            }
+        }
     }
 
     viewModel = {
@@ -38,7 +78,9 @@ define('scalejs.popup',[
         hidePopup: hidePopup,
         popupVisible: popupVisible,
         popupRegion: popupRegion,
-        modal: modal
+        modal: modal,
+        background: background,
+        position: position
     }
 
     popupRegion(template('blank_popup_template'));
--- src/scalejs.popup.js
+++ src/scalejs.popup.js
@@ -1,10 +1,12 @@
 define([
     'scalejs!core',
     'knockout',
+    'module',
     'scalejs.mvvm.views!scalejs.popup/popup'
 ], function (
     core,
-    ko
+    ko,
+    module
 ) {
     'use strict';
 
@@ -16,17 +18,55 @@ define([
         popupVisible = observable(false),
         popupRegion = observable(),
         modal = observable(),
-        viewModel;
+        background = observable(),
+        postion = observable(),
+        viewModel,
+        config = module.config;
 
     function hidePopup () {
         popupVisible(false);
         popupRegion(undefined);
+        
+        if (!config.doNotRender) {
+            var parent = document.body;
+            var oldRoot = document.getElementById("popupRoot");
+            parent.removeChild(oldRoot);
+        }
     }
 
     function renderPopup (template) {
         template.template.data.modal ? modal(true) : modal(false);
+        if (template.template.data.background === true || template.template.data.background === undefined) {
+            background('popup-background');
+        }
+        else {
+            background('popup-no-background');
+        }
+        var pos = template.template.data.position;
+        if (pos !== undefined) {
+            position('popup-box-' + pos);
+        }
+        else {
+            position('popup-box-center');
+        }
         popupRegion(template);
         popupVisible(true);
+        
+        if (!config.doNotRender) {
+            if (document.getElementById("popupRoot") === null) {
+                var newRoot = document.createElement("span");
+                newRoot.setAttribute("id","popupRoot");
+                newRoot.setAttribute("data-bind","render: popupRoot");
+                if (document.body === undefined || document.body.children === undefined) {
+                    return;
+                }
+                var parent = document.body;
+                var node = parent.children[0];
+                parent.insertBefore(newRoot, node);
+                
+                ko.applyBindings(this, document.getElementById("popupRoot"));
+            }
+        }
     }
 
     viewModel = {
@@ -34,7 +74,9 @@ define([
         hidePopup: hidePopup,
         popupVisible: popupVisible,
         popupRegion: popupRegion,
-        modal: modal
+        modal: modal,
+        background: background,
+        position: position
     }
 
     popupRegion(template('blank_popup_template'));
